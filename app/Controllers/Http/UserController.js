534809e6294984720bdcfcee29d7d401fbf9e6bf
Itacir Gabral <itacirgabral@gmail.com>
2020-06-25 17:18:44 -0400

    transaction

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,10 +1,20 @@
 'use strict'
 
 const User = use('App/Models/User')
+/*
+const Database = use('Database')
 
+const trx = Database.beginTransaction()
+const user = User.create({}, trx)
+user.addresses().createMany([], trx)
+
+await trx.commit()
+
+*/
 class UserController {
   store ({ request }) {
     return User.create(request.only(['username', 'email', 'password']))
+    // user.addresses().createMany()
   }
 }
 
